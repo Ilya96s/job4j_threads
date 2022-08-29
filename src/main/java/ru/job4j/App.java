5dcf458bd1ebf7091fe3e8209ef56510fbde58d6
Ilya96s <i.kaltygin@icloud.com>
2022-08-29 23:41:20 +0300

    Добавил .gitignore, readme.md, checkstyle.xml

--- src/main/java/ru/job4j/App.java
+++ src/main/java/ru/job4j/App.java
@@ -7,13 +7,11 @@ import org.slf4j.LoggerFactory;
  * Hello world!
  *
  */
-public class App
-{
+public class App {
 
     private static final Logger LOG = LoggerFactory.getLogger(App.class.getName());
 
-    public static void main( String[] args )
-    {
+    public static void main(String[] args) {
         LOG.trace("trace message");
         LOG.debug("debug message");
         LOG.info("info message");
