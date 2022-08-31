550553f0473ce2ea9ecc16aa950989abba5dd373
Ilya96s <i.kaltygin@icloud.com>
2022-08-31 18:45:03 +0300

    3.1.1. Threads 3. Прерывание нити

---
+++ src/main/java/ru/job4j/concurrent/ConsoleProgress.java
@@ -0,0 +1,38 @@
+package ru.job4j.concurrent;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Прерывание нити
+ * Знакомство с методами interrupt() и isInterrupted()
+ *
+ * @author Ilya Kaltygin
+ */
+public class ConsoleProgress implements Runnable {
+
+    private static final Logger LOG = LoggerFactory.getLogger(ConsoleProgress.class.getName());
+
+    @Override
+    public void run() {
+        String[] s = {"\\", "|", "/"};
+        while (!Thread.currentThread().isInterrupted()) {
+            for (String value : s) {
+                try {
+                    Thread.sleep(500);
+                    LOG.info("\r Load: {} ", value);
+                } catch (InterruptedException e) {
+                    LOG.error("InterruptedException ", e);
+                    return;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread progress = new Thread(new ConsoleProgress());
+        progress.start();
+        Thread.sleep(1000);
+        progress.interrupt();
+    }
+}
