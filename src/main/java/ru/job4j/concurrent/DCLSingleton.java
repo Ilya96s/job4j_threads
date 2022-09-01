7070ba22baee62037d9485081954d79b19b54ceb
Ilya96s <i.kaltygin@icloud.com>
2022-09-01 23:14:26 +0300

    3.1.2. Общие ресурсы 2. Модель памяти Java

---
+++ src/main/java/ru/job4j/concurrent/DCLSingleton.java
@@ -0,0 +1,24 @@
+package ru.job4j.concurrent;
+
+/**
+ * @author Ilya Kaltygin
+ */
+public final class DCLSingleton {
+
+    private static volatile DCLSingleton inst;
+
+    public static DCLSingleton instOf() {
+        if (inst == null) {
+            synchronized (DCLSingleton.class) {
+                if (inst == null) {
+                    inst = new DCLSingleton();
+                }
+            }
+        }
+        return inst;
+    }
+
+    private DCLSingleton() {
+    }
+
+}
