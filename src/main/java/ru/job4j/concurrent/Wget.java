f94ac66be050c611e4c2b186c3d2d5ac290b623d
Ilya96s <i.kaltygin@icloud.com>
2022-09-01 18:46:06 +0300

    3.1.1. Threads 4. Скачивание файла с ограничением.

--- src/main/java/ru/job4j/concurrent/Wget.java
+++ src/main/java/ru/job4j/concurrent/Wget.java
@@ -2,29 +2,73 @@ package ru.job4j.concurrent;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import java.io.BufferedInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URL;
 
 /**
  * Режим ожидания
  *
  * @author Ilya Kaltygin
  */
-public class Wget {
+public class Wget implements Runnable {
 
+    private final String url;
+    private final int speed;
     private static final Logger LOG = LoggerFactory.getLogger(Wget.class.getName());
 
-    public static void main(String[] args) {
-        Thread thread = new Thread(
-                () -> {
-                    for (int i = 0; i <= 100; i++) {
-                        try {
-                            Thread.sleep(1000);
-                            LOG.info("\rLoading : {}% ", i);
-                        } catch (InterruptedException e) {
-                            LOG.error("InterruptedException", e);
-                        }
+    public Wget(String url, int speed) {
+        this.url = url;
+        this.speed = speed;
+    }
+
+    /**
+     * Метод считывает данные из сети и если скорость считывания меньше заданной скорости, то выставляется пауза
+     */
+    @Override
+    public void run() {
+        try (BufferedInputStream in = new BufferedInputStream(new URL(url).openStream());
+             FileOutputStream fileOutputStream = new FileOutputStream("pom_tmp.xml")) {
+            byte[] dataBuffer = new byte[1024];
+            int bytesRead;
+            long start = System.currentTimeMillis();
+            while ((bytesRead = in.read(dataBuffer, 0, 1024)) != -1) {
+                long finish = System.currentTimeMillis();
+                if ((finish - start) < speed) {
+                    try {
+                        Thread.sleep(speed - (finish - start));
+                    } catch (InterruptedException e) {
+                        LOG.error("InterruptedException", e);
                     }
                 }
-        );
-        thread.start();
+                fileOutputStream.write(dataBuffer, 0, bytesRead);
+            }
+        } catch (IOException e) {
+            LOG.error("IOException", e);
+        }
+    }
+
+    /**
+     * Валидация входных параметров
+     * @param args Массив параметров
+     */
+    private static void validate(String[] args) {
+        if (args.length < 2) {
+            throw new IllegalArgumentException("Invalid input parameters. You need write 2 parameters: URL, SPEED");
+        }
+    }
+
+    public static void main(String[] args) {
+        validate(args);
+        String url = args[0];
+        int speed = Integer.parseInt(args[1]);
+        Thread wget = new Thread(new Wget(url, speed));
+        wget.start();
+        try {
+            wget.join();
+        } catch (InterruptedException e) {
+            LOG.error("InterruptedException", e);
+        }
     }
 }
