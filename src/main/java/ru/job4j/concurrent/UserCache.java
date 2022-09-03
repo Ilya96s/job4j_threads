817683d518d9f97ebe8643381db5f904a4239199
Ilya96s <i.kaltygin@icloud.com>
2022-09-03 20:26:45 +0300

    3.1.2. Общие ресурсы 4. Thread без общих ресурсов

---
+++ src/main/java/ru/job4j/concurrent/UserCache.java
@@ -0,0 +1,34 @@
+package ru.job4j.concurrent;
+
+import net.jcip.annotations.NotThreadSafe;
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+
+/**
+ * Thread без общих ресурсов
+ *
+ * Общее хранилище
+ *
+ * @author Ilya Kaltygin
+ */
+@NotThreadSafe
+public class UserCache {
+    private final ConcurrentHashMap<Integer, User> users = new ConcurrentHashMap<Integer, User>();
+    private final AtomicInteger id = new AtomicInteger();
+
+    public void add(User user) {
+        users.put(id.incrementAndGet(), User.of(user.getName()));
+    }
+
+    public User findById(int id) {
+        return User.of(users.get(id).getName());
+    }
+
+    public List<User> findAll() {
+        return users.values().stream()
+                .map(user -> User.of(user.getName()))
+                .collect(Collectors.toList());
+    }
+}
