c5055117a55d03aedad112051f169866338471fb
Ilya96s <i.kaltygin@icloud.com>
2022-09-05 22:37:57 +0300

    3.1.3. Синхронизация ресурсов 3. Денежные переводы AccountStorage

---
+++ src/main/java/ru/job4j/cash/Account.java
@@ -0,0 +1,11 @@
+package ru.job4j.cash;
+
+/**
+ * Акканут пользователя
+ *
+ * @author Ilya Kaltygin
+ */
+public record Account(int id, int amount) {
+
+}
+
---
+++ src/main/java/ru/job4j/cash/AccountStorage.java
@@ -0,0 +1,70 @@
+package ru.job4j.cash;
+
+import net.jcip.annotations.GuardedBy;
+import net.jcip.annotations.ThreadSafe;
+import java.util.HashMap;
+import java.util.Optional;
+
+/**
+ * AccountStorage - хранение счетов пользователя
+ *
+ * @author Ilya Kaltygin
+ */
+@ThreadSafe
+public class AccountStorage {
+    @GuardedBy("this")
+    private final HashMap<Integer, Account> accounts = new HashMap<>();
+
+    /**
+     * Метод добавляет новых пользователей
+     * @param account объект типа Account
+     * @return        true если добавили, иначе false
+     */
+    public synchronized boolean add(Account account) {
+        return accounts.putIfAbsent(account.id(), account) != null;
+    }
+
+    /**
+     * Метод обновляет данные пользователей
+     * @param account объект типа Account
+     * @return        true если обновили, иначе false
+     */
+    public synchronized boolean update(Account account) {
+        return accounts.replace(account.id(), account) != null;
+    }
+
+    /**
+     * Метод удаляет пользователей
+     * @param id id пользователя
+     * @return   true если удалил, иначе false
+     */
+    public synchronized boolean delete(int id) {
+        return accounts.remove(id) != null;
+    }
+
+    /**
+     * Поиск ппользователя по id
+     * @param id id пользователя
+     * @return   если объект с таким id существует, то вернуть его, иначе вернуть Optional.empty()
+     */
+    public synchronized Optional<Account> getById(int id) {
+        return Optional.ofNullable(accounts.get(id));
+    }
+
+    /**
+     * Метод осуществляет денежный перевод между двумя счетами
+     * @param fromId Счет с которого происходит перевод
+     * @param toId   Счет на который необходимо осуществить перевод
+     * @param amount Сумма перевода
+     * @return       true если перевод выполнен, иначе false;
+     */
+    public synchronized boolean transfer(int fromId, int toId, int amount) {
+        Optional<Account> fromAc = getById(fromId);
+        Optional<Account> toAc = getById(toId);
+        if (fromAc.isEmpty() || toAc.isEmpty() || fromAc.get().amount() < amount) {
+            return false;
+        }
+        return update(new Account(fromId, fromAc.get().amount() - amount))
+                && update(new Account(toId, toAc.get().amount() + amount));
+    }
+}
