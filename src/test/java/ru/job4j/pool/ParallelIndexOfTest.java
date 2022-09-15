9791d7a4993098badd2474d00da15a2ae9e0f70e
Ilya96s <i.kaltygin@icloud.com>
2022-09-15 20:44:44 +0300

    3.1.6. Пулы 3. ForkJoinPool

--- src/test/java/ru/job4j/pool/ParallelIndexOfTest.java
+++ src/test/java/ru/job4j/pool/ParallelIndexOfTest.java
@@ -14,7 +14,6 @@ class ParallelIndexOfTest {
     @Test
     void whenTypeIsAnInteger() {
         Integer[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14};
-        ParallelIndexOf<Integer> integerParallelIndexOf = new ParallelIndexOf<>(9, array, 0, array.length - 1);
         int rsl = ParallelIndexOf.findIndex(9, array);
         assertThat(rsl).isEqualTo(8);
     }
@@ -22,7 +21,6 @@ class ParallelIndexOfTest {
     @Test
     void whenTypeIsAnString() {
         String[] array = {"One", "Two", "Three", "Four", "Five", "Six", "15", "Nine", "99", "Seven", "222", "0"};
-        ParallelIndexOf<String> stringParallelIndexOf = new ParallelIndexOf<>("Five", array, 0, array.length - 1);
         int rsl = ParallelIndexOf.findIndex("15", array);
         assertThat(rsl).isEqualTo(6);
     }
@@ -30,7 +28,6 @@ class ParallelIndexOfTest {
     @Test
     void whenLinearSearch() {
         Integer[] array = {1, 2, 3, 4, 5, 6, 7};
-        ParallelIndexOf<Integer> integerParallelIndexOf = new ParallelIndexOf<>(2, array, 0, array.length - 1);
         int rsl = ParallelIndexOf.findIndex(2, array);
         assertThat(rsl).isEqualTo(1);
     }
@@ -38,7 +35,6 @@ class ParallelIndexOfTest {
     @Test
     void whenElementNotFound() {
         Integer[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14};
-        ParallelIndexOf<Integer> integerParallelIndexOf = new ParallelIndexOf<>(20, array, 0, array.length - 1);
         int rsl = ParallelIndexOf.findIndex(25, array);
         assertThat(rsl).isEqualTo(-1);
     }
