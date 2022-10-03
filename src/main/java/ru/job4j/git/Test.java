7d59e3cebc01ef75f7b22476c17982b025d3adc0
Ilya96s <i.kaltygin@icloud.com>
2022-10-03 19:22:59 +0300

    3.2. Откат изменений [#504830]

--- src/main/java/ru/job4j/git/Test.java
+++ src/main/java/ru/job4j/git/Test.java
@@ -2,6 +2,12 @@ package ru.job4j.git;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Hello world !");
+        System.out.println("Hello world!");
     }
 }
+
+
+
+
+
+
