ce5405683f80d0d8a31da8648fe23256438cdcba
Ilya96s <i.kaltygin@icloud.com>
2022-10-04 19:15:08 +0300

    3.4. Временное хранение. git stash. [#504832]

--- src/main/java/ru/job4j/git/Test.java
+++ src/main/java/ru/job4j/git/Test.java
@@ -3,6 +3,7 @@ package ru.job4j.git;
 public class Test {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Изменение №2");
     }
 }
 
